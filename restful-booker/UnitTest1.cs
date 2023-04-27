b9c57eaae490aa632459b93b1bd6c723ff682063
Serhii Khrychov <serhii.khrychov@orbussoftware.com>
2023-04-27 18:28:48 +0300

    init commit

---
+++ restful-booker/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace restful_booker
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
