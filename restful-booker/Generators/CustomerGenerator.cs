872b4ce0213431aab5da62cf797e8308660b222f
Serhii Khrychov <serhii.khrychov@orbussoftware.com>
2023-05-01 19:23:45 +0300

    (task #724) add generators part1

---
+++ restful-booker/Generators/CustomerGenerator.cs
@@ -0,0 +1,20 @@
+﻿using restful_booker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace restful_booker.Generators
+{
+    public class CustomerGenerator
+    {
+        public static Customer GenerateCustomer()
+        {
+            var firstName = "John";
+            var lastName = "Doe";
+
+            return new Customer { FirstName = firstName, LastName = lastName };
+        }
+    }
+}
