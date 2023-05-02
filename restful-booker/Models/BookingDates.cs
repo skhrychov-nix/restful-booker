e7fb5e192057e8cb4c46e3a4a1779e21f2e3d1a3
Serhii <sergeyhall@gmail.com>
2023-05-02 17:57:23 +0200

    (task #721) remove redundant using

--- restful-booker/Models/BookingDates.cs
+++ restful-booker/Models/BookingDates.cs
@@ -1,10 +1,5 @@
 ﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Xml.Serialization;
 
 namespace restful_booker.Models
