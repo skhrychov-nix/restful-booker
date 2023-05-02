c9b327917c9306687e9a3964b9457b2de04016a8
Serhii Khrychov <serhii.khrychov@orbussoftware.com>
2023-05-02 12:55:46 +0300

    (task #724) add credantials generator

---
+++ restful-booker/Generators/CredentialsGenerator.cs
@@ -0,0 +1,24 @@
+﻿using restful_booker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace restful_booker.Generators
+{
+    public class CredentialsGenerator
+    {
+        public static Credentials GenerateCredentils()
+        {
+            var userName = "Admin";
+            var password = "password123";
+
+            return new Credentials
+            {
+                UserName = userName,
+                Password = password
+            };
+        }
+    }
+}
