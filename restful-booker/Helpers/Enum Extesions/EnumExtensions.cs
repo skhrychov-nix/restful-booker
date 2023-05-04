e72bf32d7aee0a0de563200fca694dd7fe643119
Serhii <sergeyhall@gmail.com>
2023-05-04 11:33:50 +0200

    (task #712) add enum and extention to this enum

---
+++ restful-booker/Helpers/Enum Extesions/EnumExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel;
+using DescriptionAttribute = System.ComponentModel.DescriptionAttribute;
+
+namespace restful_booker.Helpers.Enum_Extesions;
+
+public static class EnumExtensions
+{
+    public static string GetDescription(this Enum value)
+    {
+        var type = value.GetType();
+        var name = Enum.GetName(type, value);
+        var field = type.GetField(name);
+        var attribute =
+            Attribute.GetCustomAttribute(field, typeof(DescriptionAttribute)) as
+                System.ComponentModel.DescriptionAttribute;
+        return attribute != null ? attribute.Description : name;
+    }
+
+    public static T? GetEnumValueFromDescription<T>(string description) where T : Enum
+    {
+        foreach (var field in typeof(T).GetFields())
+        {
+            if (Attribute.GetCustomAttribute(field, typeof(DescriptionAttribute)) is DescriptionAttribute attribute)
+            {
+                if (attribute.Description == description)
+                {
+                    return (T) field.GetValue(null);
+                }
+            }
+            else if (field.Name == description)
+            {
+                return (T)Enum.Parse(typeof(T), description);
+            }
+        }
+
+        throw new ArgumentException($"Enum value with the description {description} was not found.");
+    }
+}
+
+
---
+++ restful-booker/Helpers/MediaType.cs
@@ -0,0 +1,12 @@
+﻿namespace restful_booker.Helpers;
+
+public enum MediaType
+{
+    None,
+    [System.ComponentModel.Description("application/json")]
+    Json,
+    [System.ComponentModel.Description("application/xml")]
+    Xml,
+    [System.ComponentModel.Description("text/plain")]
+    PlainText
+}
